32b03df8fa1a5732ec3db36d9b21e9a2f3b8f139
henry <ah13441673@gmail.com>
2017-11-10 17:53:10 +0800

    171110-2

--- src/core/content/browser/views (副本).py
+++ src/core/content/browser/views (副本).py
@@ -9,6 +9,7 @@ from selenium import webdriver
 import sys
 from Products.CMFPlone.utils import safe_unicode
 from plone import api
+import cPickle as pickle 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -60,57 +61,68 @@ class Manage_list(BrowserView):
 
 class Release_news(BrowserView):
     def __call__(self):
+        self.web_user = api.user.get_current().getProperty('fullname')
+        
         self.news_title = safe_unicode(self.request.get('news_title'))
         self.news_content = safe_unicode(self.request.get('news_content'))
         self.btn_content = self.request.get('btn_content')
         self.news_url = self.request.get('news_url')
-        self.web_user = api.user.get_current().getProperty('fullname')
-
-        self.target = 'https://www.weibo.com/login.php'
-
-        self.release = self.release_news(self.target, self.news_title, self.news_content, self.btn_content, self.news_url, self.web_user)
-
-    def release_news(self, target, news_title, news_content, btn_content , news_url, web_user):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('window-size=1200,1100');
-        chromedriver = "/usr/local/share/chromedriver"
-        browser = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver)
-        data_list = []
-
-        browser.maximize_window()
-
-        browser.implicitly_wait(10)
-        browser.get(target)
-
-        print browser.title
-
-        username = browser.find_element_by_id('loginname')
-        pwd = browser.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[2]/div/input')
-        print ('輸入帳號密碼')
-        username.send_keys('ah13441673@gmail.com')
-        time.sleep(1)
-        pwd.send_keys('hn13441673')
-        time.sleep(1)
-        browser.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[6]/a').click()
-        print ('按下登入紐')
-        # 數入訊息
-        time.sleep(2)
-        print ('輸入訊息')
-        send_box = browser.find_element_by_xpath('//textarea[@class="W_input"]')
-
-        send_box.send_keys(news_title)
-        time.sleep(1)
-        send_box.send_keys(news_content)
-        time.sleep(1)
-        # 按下送出訊息
-        browser.find_element_by_xpath('//div[@id="v6_pl_content_publishertop"]/div/div[3]/div[1]/a').click()
         
-        browser.quit()
-        if btn_content == "發佈":
-            self.insert_web_user(news_url, web_user)
+        self.target = 'https://www.weibo.com/login.php'
+        self.check_count(self.web_user , self.target, self.news_title, self.news_content,
+                     self.btn_content, self.news_url)
+    def release_news(self, target, news_title, news_content, btn_content , news_url, web_user, filename):
+        # chrome_options = webdriver.ChromeOptions()
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--disable-gpu')
+        # chrome_options.add_argument('window-size=1200,1100');
+        # chromedriver = "/usr/local/share/chromedriver"
+        # browser = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver)
+        # data_list = []
 
+        # browser.maximize_window()
+
+        # browser.implicitly_wait(10)
+        # browser.get(target)
+
+        # print browser.title
+
+        # username = browser.find_element_by_id('loginname')
+        # pwd = browser.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[2]/div/input')
+        # print ('輸入帳號密碼')
+        # username.send_keys('ah13441673@gmail.com')
+        # time.sleep(1)
+        # pwd.send_keys('hn13441673')
+        # time.sleep(1)
+        # browser.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[6]/a').click()
+        # print ('按下登入紐')
+        # # 數入訊息
+        # time.sleep(2)
+        # print ('輸入訊息')
+        # send_box = browser.find_element_by_xpath('//textarea[@class="W_input"]')
+
+        # send_box.send_keys(news_title)
+        # time.sleep(1)
+        # send_box.send_keys(news_content)
+        # time.sleep(1)
+        # # 按下送出訊息
+        # browser.find_element_by_xpath('//div[@id="v6_pl_content_publishertop"]/div/div[3]/div[1]/a').click()
+        
+        # browser.quit()
+        # if btn_content == "發佈":
+        #     self.insert_web_user(news_url, web_user)
+        time.sleep(3)
+        f = open(filename,'rb')
+        
+        count = pickle.load(f)
+        print('進行到'+str(count[web_user]))
+        f.close()
+        f = open(filename,'wb')
+        count[web_user]-=1
+        print('刪除程式'+str(count[web_user]))
+        pickle.dump(count,f)
+        f.close()
+        time.sleep(1.5)
         print('發佈完成')
 
     def insert_web_user(self, news_url, web_user):
@@ -162,3 +174,34 @@ class Release_news(BrowserView):
         finally:
             connection.close()
     
+    
+
+
+    def check_count(self, web_user, news_title, news_content, btn_content, news_url, target):
+        filename=web_user+'.pickle'
+        try:
+            f = open (filename,'rb')
+            f.close()
+            
+            self.add_count(web_user, news_title, news_content, btn_content, news_url, target, filename)
+        except:
+            f = open(filename,'wb')
+            pickle.dump({web_user:0},f)
+        
+            f.close()
+            self.add_count(web_user, news_title, news_content, btn_content, news_url, target, filename)
+
+    def add_count(self, web_user, news_title, news_content, btn_content, news_url, target, filename):
+        f = open (filename,'rb')
+        count = pickle.load(f)
+        f.close()
+        if count[web_user] < 5:
+            f= open(filename,'wb')
+            
+            count[web_user]+=1
+            print('現在進行'+str(count[web_user]))
+            pickle.dump(count,f)
+            f.close()
+            self.release_news(target, news_title, news_content, btn_content, news_url, web_user, filename)
+        else:
+            print ('too many')
